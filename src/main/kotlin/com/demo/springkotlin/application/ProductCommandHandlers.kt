4f0548e95043b36d760d0fe78ca7149410d631c7
Allan <al_hardy@live.com.au>
2019-10-22 22:19:08 +1100

    publish new product created event

--- src/main/kotlin/com/demo/springkotlin/application/ProductCommandHandlers.kt
+++ src/main/kotlin/com/demo/springkotlin/application/ProductCommandHandlers.kt
@@ -1,10 +1,12 @@
 package com.demo.springkotlin.application
 
 import com.demo.springkotlin.application.commands.AddProductCommand
+import com.demo.springkotlin.domain.NewProductCreated
 import com.demo.springkotlin.domain.ProductAggregate
 import com.demo.springkotlin.domain.ProductRepository
 import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.eventhandling.EventBus
+import org.axonframework.eventhandling.GenericEventMessage
 import java.util.*
 
 open class ProductCommandHandlers(
@@ -16,6 +18,8 @@ open class ProductCommandHandlers(
 
         val product = ProductAggregate(productId, command.name, command.description)
         productRepository.save(product)
+
+        eventBus.publish(GenericEventMessage.asEventMessage<NewProductCreated>(NewProductCreated(productId)))
 
         return productId
     }
---
+++ src/main/kotlin/com/demo/springkotlin/domain/NewProductCreated.kt
@@ -0,0 +1,5 @@
+package com.demo.springkotlin.domain
+
+import java.util.*
+
+data class NewProductCreated(val productId: UUID)
